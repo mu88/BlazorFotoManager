0fa6bbe28f704ba0889c292022c8a8b435aea609
mu88 <mu88_developer@outlook.com>
2019-08-09 14:24:32 +0200

    Remove unncessary template content

--- FotoManager/Startup.cs
+++ FotoManager/Startup.cs
@@ -1,6 +1,5 @@
 using System.Threading.Tasks;
 using ElectronNET.API;
-using FotoManager.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -24,7 +23,6 @@ namespace FotoManager
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
